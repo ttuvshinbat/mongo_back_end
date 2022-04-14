8bdad404c4ef10fece99349aa76311c7b6ddaf4b
Mstars_Lab1_12 <mstars_lab1_12@Mstars-Lab1-12s-iMac.local>
2022-04-14 15:44:59 +0800

    changed

--- mongo/controller/Authcontroller.js
+++ mongo/controller/Authcontroller.js
@@ -13,7 +13,7 @@ const register = async (req, res, next) => {
     }
     var hashedPassword = await bcrypt.hash(data.password, 10);
     data.password = hashedPassword;
-    data.role == 0 ? (data.role_id = 1) : (data.role_id = data.role);
+    data.role_id == 0 ? (data.role_id = 1) : (data.role_id = data.role);
     data.created_date = Date("Y-m-d");
     data.last_activity = Date("Y-m-d h:m:s");
     email = data.email;
